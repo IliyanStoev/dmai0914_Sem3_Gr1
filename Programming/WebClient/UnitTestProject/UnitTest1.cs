3dab8f3319cb775b7df05001b8120579994bd9f5
Cosmin <cosmingabinat@yahoo.com>
2015-11-12 12:01:45 +0100

    mORE CHANGES
    With default page design and the login test working !!!

---
+++ Programming/WebClient/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestProject.ServiceReference1;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string userName = "Anna";
+            string password = "2222".GetHashCode().ToString(); 
+
+            Service1Client client = new ServiceReference1.Service1Client();
+
+            Assert.IsNotNull(client.Login(userName, password));
+        }
+    }
+}
