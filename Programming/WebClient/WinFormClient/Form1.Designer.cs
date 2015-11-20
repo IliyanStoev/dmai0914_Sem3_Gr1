3b31fd303972f2bc2c679ac4969b4422890e33f4
piskeflode <cimaskevic.nikita@gmailcom>
2015-11-20 10:56:06 +0100

    Pizdec

--- Programming/WebClient/WinFormClient/Form1.Designer.cs
+++ Programming/WebClient/WinFormClient/Form1.Designer.cs
@@ -28,20 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameLbl = new System.Windows.Forms.Label();
+            this.passLbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AssignmentBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(204, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to HomeworkOnOp";
+            // 
+            // userNameLbl
+            // 
+            this.userNameLbl.AutoSize = true;
+            this.userNameLbl.Location = new System.Drawing.Point(195, 83);
+            this.userNameLbl.Name = "userNameLbl";
+            this.userNameLbl.Size = new System.Drawing.Size(55, 13);
+            this.userNameLbl.TabIndex = 1;
+            this.userNameLbl.Text = "Username";
+            // 
+            // passLbl
+            // 
+            this.passLbl.AutoSize = true;
+            this.passLbl.Location = new System.Drawing.Point(195, 116);
+            this.passLbl.Name = "passLbl";
+            this.passLbl.Size = new System.Drawing.Size(53, 13);
+            this.passLbl.TabIndex = 2;
+            this.passLbl.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(289, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Invalid user password";
+            this.label4.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(292, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(317, 192);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 6;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.AssignmentBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(688, 457);
+            this.panel1.TabIndex = 7;
+            this.panel1.Visible = false;
+            // 
+            // AssignmentBtn
+            // 
+            this.AssignmentBtn.Location = new System.Drawing.Point(59, 56);
+            this.AssignmentBtn.Name = "AssignmentBtn";
+            this.AssignmentBtn.Size = new System.Drawing.Size(75, 23);
+            this.AssignmentBtn.TabIndex = 0;
+            this.AssignmentBtn.Text = "Assignment";
+            this.AssignmentBtn.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Homeworks";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 424);
+            this.ClientSize = new System.Drawing.Size(688, 457);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.passLbl);
+            this.Controls.Add(this.userNameLbl);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Dekstop App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label userNameLbl;
+        private System.Windows.Forms.Label passLbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button AssignmentBtn;
     }
 }
 
--- Programming/WebClient/WinFormClient/Form1.cs
+++ Programming/WebClient/WinFormClient/Form1.cs
@@ -8,13 +8,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using WinFormClient.WCFWebReference;
+
+
+
 namespace WinFormClient
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public static Person pers;
+       
+         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+
+
+            Service1Client service = new WCFWebReference.Service1Client();
+            Person pers;
+
+            string userName = textBox1.Text;
+            string password = textBox2.Text;
+
+             pers = service.Login(userName, password);
+
+            if (pers != null)
+            {
+                panel1.Visible = true;
+                
+            }
+            else
+            {
+                label4.Visible = true;
+            }
+
+
+
         }
     }
 }
