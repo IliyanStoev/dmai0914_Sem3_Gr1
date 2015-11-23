ce1458e2a57a47aea82b753e038466c0dfdeb352
janix89 <janisindrans7@inbox.lv>
2015-11-23 08:58:36 +0100

    Janis

--- Programming/WebClient/WinFormClient/Form1.Designer.cs
+++ Programming/WebClient/WinFormClient/Form1.Designer.cs
@@ -36,8 +36,9 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.loginBtn = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.AssignmentBtn = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.AssignmentBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,27 +48,30 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(204, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(238, 20);
+            this.label1.Size = new System.Drawing.Size(291, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to HomeworkOnOp";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // userNameLbl
             // 
             this.userNameLbl.AutoSize = true;
             this.userNameLbl.Location = new System.Drawing.Point(195, 83);
             this.userNameLbl.Name = "userNameLbl";
-            this.userNameLbl.Size = new System.Drawing.Size(55, 13);
+            this.userNameLbl.Size = new System.Drawing.Size(73, 17);
             this.userNameLbl.TabIndex = 1;
             this.userNameLbl.Text = "Username";
+            this.userNameLbl.Click += new System.EventHandler(this.userNameLbl_Click);
             // 
             // passLbl
             // 
             this.passLbl.AutoSize = true;
             this.passLbl.Location = new System.Drawing.Point(195, 116);
             this.passLbl.Name = "passLbl";
-            this.passLbl.Size = new System.Drawing.Size(53, 13);
+            this.passLbl.Size = new System.Drawing.Size(69, 17);
             this.passLbl.TabIndex = 2;
             this.passLbl.Text = "Password";
+            this.passLbl.Click += new System.EventHandler(this.passLbl_Click);
             // 
             // label4
             // 
@@ -75,24 +79,27 @@
             this.label4.ForeColor = System.Drawing.Color.Red;
             this.label4.Location = new System.Drawing.Point(289, 157);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.Size = new System.Drawing.Size(144, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Invalid user password";
             this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(292, 83);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(292, 116);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
             this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // loginBtn
             // 
@@ -106,6 +113,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.AssignmentBtn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -114,28 +122,45 @@
             this.panel1.Size = new System.Drawing.Size(688, 457);
             this.panel1.TabIndex = 7;
             this.panel1.Visible = false;
-            // 
-            // AssignmentBtn
-            // 
-            this.AssignmentBtn.Location = new System.Drawing.Point(59, 56);
-            this.AssignmentBtn.Name = "AssignmentBtn";
-            this.AssignmentBtn.Size = new System.Drawing.Size(75, 23);
-            this.AssignmentBtn.TabIndex = 0;
-            this.AssignmentBtn.Text = "Assignment";
-            this.AssignmentBtn.UseVisualStyleBackColor = true;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(59, 79);
+            this.button2.Location = new System.Drawing.Point(285, 177);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(107, 29);
             this.button2.TabIndex = 1;
             this.button2.Text = "Homeworks";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // AssignmentBtn
+            // 
+            this.AssignmentBtn.Location = new System.Drawing.Point(285, 145);
+            this.AssignmentBtn.Name = "AssignmentBtn";
+            this.AssignmentBtn.Size = new System.Drawing.Size(107, 29);
+            this.AssignmentBtn.TabIndex = 0;
+            this.AssignmentBtn.Text = "Assignment";
+            this.AssignmentBtn.UseVisualStyleBackColor = true;
+            this.AssignmentBtn.Click += new System.EventHandler(this.AssignmentBtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(688, 457);
+            this.panel2.TabIndex = 2;
+            //
+            // label5
+            //
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            //
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(688, 457);
             this.Controls.Add(this.panel1);
@@ -147,7 +172,7 @@
             this.Controls.Add(this.userNameLbl);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Dekstop App";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -169,6 +194,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button AssignmentBtn;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
